4344fdce1e5d3e778a2f39eee59b96fa054161e8
enningxie <enningxie@163.com>
2017-04-05 21:45:42 +0800

    chapter1

---
+++ chapter1/scrapetest.py
@@ -0,0 +1,23 @@
+# 初见网络爬虫
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+from bs4 import BeautifulSoup
+
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except (HTTPError, URLError) as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(), "lxml")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle("http://pythonscraping.com/pages/page1.html")
+if title is None:
+    print("Title could not be found")
+else:
+    print(title)
