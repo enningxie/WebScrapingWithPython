681c58297899d161d9c1d8f995fde3314c4ce2d2
enningxie <enningxie@163.com>
2018-04-17 10:30:48 +0800

    data_augmentation

--- chapter1/Tiger/data_augmentation.py
+++ chapter1/Tiger/data_augmentation.py
@@ -8,21 +8,25 @@ import argparse
 from math import pi
 from PIL import Image
 import os
-# import cv2
+import cv2
 
 
-IMAGE_SIZE = 500
+IMAGE_SIZE = 224
 IMAGES = 4
 
 
 def arg_parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('--file_path_', type=str,
-                        default=r'D:\DataSets\crop_img\crop_img')
-    parser.add_argument('--img_png', type=str,
-                        default=r'D:\DataSets\crop_img\img_png')
-    parser.add_argument('--file_path', type=str, default='/home/enningxie/Documents/DataSets/butterfly_/data_augmentation')
-    parser.add_argument('--process_data', type=str, default='/home/enningxie/Documents/DataSets/butterfly_/process_data')
+                        default='/home/enningxie/Documents/DataSets/crop_img')
+    parser.add_argument('--test_path', type=str,
+                        default='/home/enningxie/Documents/DataSets/butterfly_/convert_')
+    parser.add_argument('--test_result', type=str,
+                        default='/home/enningxie/Documents/DataSets/butterfly_/test_path')
+    parser.add_argument('--test_convert', type=str,
+                        default='/home/enningxie/Documents/DataSets/butterfly_/test_convert')
+    parser.add_argument('--file_path', type=str, default='/home/enningxie/Documents/DataSets/butterfly_/process_data')
+    parser.add_argument('--process_data', type=str, default='/home/enningxie/Documents/DataSets/butterfly_/data_augmentation')
     return parser.parse_args()
 
 
@@ -35,9 +39,16 @@ def img_file_path(path):
 
 
 # save img
-def img_save_op(img_data, path, step):
+def img_save_op(img_data, img_label, path, step):
     for index, img in enumerate(img_data):
-        mpimg.imsave(os.path.join(path, str(step) + '_'+ str(index) + '.png'), img)
+        mpimg.imsave(os.path.join(path, img_label[index] + '_' + str(step) + '_' + str(index) + '.png'), img)
+    print('done.')
+
+
+# save img
+def img_save_op_(img_data, path, step):
+    for index, img in enumerate(img_data):
+        mpimg.imsave(os.path.join(path, str(step) + '_' + str(index) + '.png'), img)
     print('done.')
 
 
@@ -45,19 +56,21 @@ def convert_jpg_to_png(path, to_path):
     for filename in os.listdir(path):
         if os.path.splitext(filename)[-1] == '.jpg':
             # print(filename)
-            img = Image.open(path + '\\' + filename)
+            img = cv2.imread(os.path.join(path, filename))
+            # img = Image.open(path + '\\' + filename)
             # # print(filename.replace(".jpg", ".png"))
             newfilename = filename.replace(".jpg", ".png")
-            img.save(to_path + '\\' + newfilename)
-    print('done.')
+            # img.save(to_path + '\\' + newfilename)
             # # cv2.imshow("Image",img)
             # # cv2.waitKey(0)
-            # cv2.imwrite(FLAGS.file_path + newfilename, img)
+            cv2.imwrite(os.path.join(to_path, newfilename), img)
+    print('convert done.')
 
 
 # resize the images
 def tf_resize_images(X_img_file_paths):
     X_data = []
+    X_label = []
     tf.reset_default_graph()
     X = tf.placeholder(tf.float32, (None, None, 3))
     tf_img = tf.image.resize_images(X, (IMAGE_SIZE, IMAGE_SIZE),
@@ -70,12 +83,15 @@ def tf_resize_images(X_img_file_paths):
             img = mpimg.imread(file_path)[:, :, :3]  # Do not read alpha channel.
             resized_img = sess.run(tf_img, feed_dict={X: img})
             X_data.append(resized_img)
+            X_label.append(file_path.split('/')[-1][:11])
 
     X_data = np.array(X_data, dtype=np.float32)  # Convert to numpy
-    return X_data
+    X_label = np.array(X_label, dtype=np.str)
+    return X_data, X_label
+
 
 # scaling
-def central_scale_images(X_imgs, scales):
+def central_scale_images(X_imgs, X_label, scales):
     # Various settings needed for Tensorflow operation
     boxes = np.zeros((len(scales), 4), dtype=np.float32)
     for index, scale in enumerate(scales):
@@ -86,6 +102,7 @@ def central_scale_images(X_imgs, scales):
     crop_size = np.array([IMAGE_SIZE, IMAGE_SIZE], dtype=np.int32)
 
     X_scale_data = []
+    X_label_ = []
     tf.reset_default_graph()
     X = tf.placeholder(tf.float32, shape=(1, IMAGE_SIZE, IMAGE_SIZE, 3))
     # Define Tensorflow operation for all scales but only one base image at a time
@@ -93,13 +110,17 @@ def central_scale_images(X_imgs, scales):
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
-        for img_data in X_imgs:
+        for index, img_data in enumerate(X_imgs):
             batch_img = np.expand_dims(img_data, axis=0)
             scaled_imgs = sess.run(tf_img, feed_dict={X: batch_img})
             X_scale_data.extend(scaled_imgs)
+            for _ in range(len(scales)):
+                X_label_.append(X_label[index])
+
 
     X_scale_data = np.array(X_scale_data, dtype=np.float32)
-    return X_scale_data
+    X_label_ = np.array(X_label_, dtype=np.str)
+    return X_scale_data, X_label_
 
 
 # Translation
@@ -136,10 +157,11 @@ def get_translate_parameters(index):
     return offset, size, w_start, w_end, h_start, h_end
 
 
-def translate_images(X_imgs):
+def translate_images(X_imgs, X_label):
     offsets = np.zeros((len(X_imgs), 2), dtype=np.float32)
     n_translations = 4
     X_translated_arr = []
+    X_label_ = []
 
     tf.reset_default_graph()
     with tf.Session() as sess:
@@ -156,31 +178,38 @@ def translate_images(X_imgs):
             X_translated[:, h_start: h_start + size[0], \
             w_start: w_start + size[1], :] = glimpses
             X_translated_arr.extend(X_translated)
+            X_label_.extend(X_label)
+
     X_translated_arr = np.array(X_translated_arr, dtype=np.float32)
-    return X_translated_arr
+    X_label_ = np.array(X_label_, dtype=np.str)
+    return X_translated_arr, X_label_
 
 
 # Rotation at 90 degrees
-def rotate_images(X_imgs):
+def rotate_images(X_imgs, X_label):
     X_rotate = []
+    X_label_ = []
     tf.reset_default_graph()
     X = tf.placeholder(tf.float32, shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
     k = tf.placeholder(tf.int32)
     tf_img = tf.image.rot90(X, k=k)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for img in X_imgs:
+        for index, img in enumerate(X_imgs):
             for i in range(3):  # Rotation at 90, 180 and 270 degrees
                 rotated_img = sess.run(tf_img, feed_dict={X: img, k: i + 1})
                 X_rotate.append(rotated_img)
+                X_label_.append(X_label[index])
 
     X_rotate = np.array(X_rotate, dtype=np.float32)
-    return X_rotate
+    X_label_ = np.array(X_label_, dtype=np.str)
+    return X_rotate, X_label_
 
 
 # Rotation at finer angles
-def rotate_images_(X_imgs, start_angle, end_angle, n_images):
+def rotate_images_(X_imgs, X_label, start_angle, end_angle, n_images):
     X_rotate = []
+    X_label_ = []
     iterate_at = (end_angle - start_angle) / (n_images - 1)
 
     tf.reset_default_graph()
@@ -196,14 +225,17 @@ def rotate_images_(X_imgs, start_angle, end_angle, n_images):
             radian_arr = [radian_value] * len(X_imgs)
             rotated_imgs = sess.run(tf_img, feed_dict={X: X_imgs, radian: radian_arr})
             X_rotate.extend(rotated_imgs)
+            X_label_.extend(X_label)
 
     X_rotate = np.array(X_rotate, dtype=np.float32)
-    return X_rotate
+    X_label_ = np.array(X_label_, dtype=np.str)
+    return X_rotate, X_label_
 
 
 # flipping
-def flip_images(X_imgs):
+def flip_images(X_imgs, X_label):
     X_flip = []
+    X_label_ = []
     tf.reset_default_graph()
     X = tf.placeholder(tf.float32, shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
     tf_img1 = tf.image.flip_left_right(X)
@@ -211,11 +243,14 @@ def flip_images(X_imgs):
     tf_img3 = tf.image.transpose_image(X)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for img in X_imgs:
+        for index, img in enumerate(X_imgs):
             flipped_imgs = sess.run([tf_img1, tf_img2, tf_img3], feed_dict={X: img})
             X_flip.extend(flipped_imgs)
+            for _ in range(3):
+                X_label_.append(X_label[index])
     X_flip = np.array(X_flip, dtype=np.float32)
-    return X_flip
+    X_label_ = np.array(X_label_, dtype=np.str)
+    return X_flip, X_label_
 
 
 # Adding Salt and Pepper noise
@@ -290,38 +325,43 @@ def perspective_transform(X_img):
 
 if __name__ == '__main__':
     FLAGS = arg_parse()
-    # path = img_file_path(FLAGS.file_path)
-    # X_imgs = tf_resize_images(path)
-    # step 0: resize
-    # img_save_op(X_imgs, FLAGS.process_data, 0)
-    # # step 1: scale
-    # # Produce each image at scaling of 90%, 75% and 60% of original image.
-    # scaled_imgs = central_scale_images(X_imgs, [0.90, 0.75, 0.60])
-    # img_save_op(scaled_imgs, FLAGS.process_data, 1)
-    # # step 2: Translate
-    # translated_imgs = translate_images(X_imgs)
-    # img_save_op(translated_imgs, FLAGS.process_data, 2)
-    # # step 3: Rotation at 90 degrees
-    # rotated_imgs = rotate_images(X_imgs)
-    # img_save_op(rotated_imgs, FLAGS.process_data, 3)
-    # # step 4: Rotation at finer angles
-    # # Start rotation at -90 degrees, end at 90 degrees and produce totally 14 images
-    # rotated_imgs_ = rotate_images_(X_imgs, -90, 90, 14)
-    # img_save_op(rotated_imgs_, FLAGS.process_data, 4)
-    # # step 5: flipping
-    # flipped_images = flip_images(X_imgs)
-    # img_save_op(flipped_images, FLAGS.process_data, 5)
-    # # step 6: Adding Salt and Pepper noise
-    # salt_pepper_noise_imgs = add_salt_pepper_noise(X_imgs)
-    # img_save_op(salt_pepper_noise_imgs, FLAGS.process_data, 6)
-    # # step 7: Lighting condition by adding Gaussian noise
-    # gaussian_noise_imgs = add_gaussian_noise(X_imgs)
-    # img_save_op(gaussian_noise_imgs, FLAGS.process_data, 7)
-    # step 8:
-    # perspective_img = perspective_transform(X_imgs[0])
-    # img_save_op(perspective_img, FLAGS.process_data, 8)
+    origin_path = FLAGS.file_path
+    to_path = FLAGS.process_data
     # step -1
-    convert_jpg_to_png(FLAGS.file_path_, FLAGS.img_png)
+    # convert_jpg_to_png(FLAGS.test_path, FLAGS.test_convert)
+    path = img_file_path(origin_path)
+    # print(path[0].split('/')[-1][:11])
+    # step 0: resize
+    X_imgs, X_label0 = tf_resize_images(path)
+    img_save_op(X_imgs, X_label0, to_path, 0)
+    # step 1: scale
+    # Produce each image at scaling of 90%, 75% and 60% of original image.
+    scaled_imgs, X_label1 = central_scale_images(X_imgs, X_label0, [0.90, 0.75, 0.60])
+    img_save_op(scaled_imgs, X_label1, to_path, 1)
+    # # step 2: Translate
+    translated_imgs, X_label2 = translate_images(X_imgs, X_label0)
+    img_save_op(translated_imgs, X_label2, to_path, 2)
+    # step 3: Rotation at 90 degrees
+    rotated_imgs, X_label3 = rotate_images(X_imgs, X_label0)
+    img_save_op(rotated_imgs, X_label2, to_path, 3)
+    # step 4: Rotation at finer angles
+    # Start rotation at -90 degrees, end at 90 degrees and produce totally 14 images
+    rotated_imgs_, X_label4 = rotate_images_(X_imgs, X_label0, -90, 90, 14)
+    img_save_op(rotated_imgs_, X_label4, to_path, 4)
+    # step 5: flipping
+    flipped_images, X_label5 = flip_images(X_imgs, X_label0)
+    img_save_op(flipped_images, X_label5, to_path, 5)
+    # step 6: Adding Salt and Pepper noise
+    salt_pepper_noise_imgs = add_salt_pepper_noise(X_imgs)
+    img_save_op(salt_pepper_noise_imgs, X_label0, to_path, 6)
+    # step 7: Lighting condition by adding Gaussian noise
+    gaussian_noise_imgs = add_gaussian_noise(X_imgs)
+    img_save_op(gaussian_noise_imgs, X_label0, to_path, 7)
+    # # step 8:
+    # # perspective_img = perspective_transform(X_imgs[0])
+    # # img_save_op(perspective_img, FLAGS.process_data, 8)
+
+
 
 
 
